5b912ed944435ce2704926a0c73a131108021e13
ais93 <aismcauley00@gmail.com>
2016-04-09 10:39:42 +0000

    finish js effect

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -89,9 +89,188 @@ signin.onclick = function() {
 	rform.style.display = "none";
 }
 
+//jquery for flower hover effect bouquet
+		$('#d111').contenthover({
+			overlay_background:'#000',
+			overlay_opacity:0.8
+		});
 
+		$('#d112').contenthover({
+			overlay_background:'#000',
+			overlay_opacity:0.8
+		});
 
+		$('#d113').contenthover({
+			overlay_background:'#000',
+			overlay_opacity:0.8
+		});
 
+		$('#d114').contenthover({
+			overlay_background:'#000',
+			overlay_opacity:0.8
+		});
+
+		$('#d115').contenthover({
+			overlay_background:'#000',
+			overlay_opacity:0.8
+		});
+
+		$('#d116').contenthover({
+			overlay_background:'#000',
+			overlay_opacity:0.8
+		});
+
+		$('#d117').contenthover({
+			overlay_background:'#000',
+			overlay_opacity:0.8
+		});
+
+		$('#d118').contenthover({
+			overlay_background:'#000',
+			overlay_opacity:0.8
+		});
+
+		$('#d119').contenthover({
+			overlay_background:'#000',
+			overlay_opacity:0.8
+		});
+
+		$('#d120').contenthover({
+			overlay_background:'#000',
+			overlay_opacity:0.8
+		});
+
+		$('#d121').contenthover({
+			overlay_background:'#000',
+			overlay_opacity:0.8
+		});
+
+		$('#d122').contenthover({
+			overlay_background:'#000',
+			overlay_opacity:0.8
+		});
+		
+		$('#d11').contenthover({
+			overlay_background:'#000',
+			overlay_opacity:0.8
+		});
+
+//jquery for flower hover effect anniversary
+		$('#d12').contenthover({
+			overlay_background:'#000',
+			overlay_opacity:0.8
+		});
+
+		$('#d13').contenthover({
+			overlay_background:'#000',
+			overlay_opacity:0.8
+		});
+
+		$('#d14').contenthover({
+			overlay_background:'#000',
+			overlay_opacity:0.8
+		});
+
+		$('#d15').contenthover({
+			overlay_background:'#000',
+			overlay_opacity:0.8
+		});
+
+		$('#d16').contenthover({
+			overlay_background:'#000',
+			overlay_opacity:0.8
+		});
+
+		$('#d17').contenthover({
+			overlay_background:'#000',
+			overlay_opacity:0.8
+		});
+
+		$('#d18').contenthover({
+			overlay_background:'#000',
+			overlay_opacity:0.8
+		});
+
+		$('#d19').contenthover({
+			overlay_background:'#000',
+			overlay_opacity:0.8
+		});
+
+		$('#d20').contenthover({
+			overlay_background:'#000',
+			overlay_opacity:0.8
+		});
+
+		$('#d21').contenthover({
+			overlay_background:'#000',
+			overlay_opacity:0.8
+		});
+
+		$('#d22').contenthover({
+			overlay_background:'#000',
+			overlay_opacity:0.8
+		});
+
+//jquery for flower hover effect funeral
+		$('#d1').contenthover({
+			overlay_background:'#000',
+			overlay_opacity:0.8
+		});
+
+		$('#d2').contenthover({
+			overlay_background:'#000',
+			overlay_opacity:0.8
+		});
+
+		$('#d3').contenthover({
+			overlay_background:'#000',
+			overlay_opacity:0.8
+		});
+
+		$('#d4').contenthover({
+			overlay_background:'#000',
+			overlay_opacity:0.8
+		});
+
+		$('#d5').contenthover({
+			overlay_background:'#000',
+			overlay_opacity:0.8
+		});
+
+		$('#d6').contenthover({
+			overlay_background:'#000',
+			overlay_opacity:0.8
+		});
+
+		$('#d7').contenthover({
+			overlay_background:'#000',
+			overlay_opacity:0.8
+		});
+
+		$('#d8').contenthover({
+			overlay_background:'#000',
+			overlay_opacity:0.8
+		});
+
+		$('#d9').contenthover({
+			overlay_background:'#000',
+			overlay_opacity:0.8
+		});
+
+		$('#d10').contenthover({
+			overlay_background:'#000',
+			overlay_opacity:0.8
+		});
+
+		$('#d11').contenthover({
+			overlay_background:'#000',
+			overlay_opacity:0.8
+		});
+
+		$('#d12').contenthover({
+			overlay_background:'#000',
+			overlay_opacity:0.8
+		});
 
 
 
