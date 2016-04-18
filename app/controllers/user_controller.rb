3195435a1b7ff9c7ff412673333bfff101adf286
ais93 <aismcauley00@gmail.com>
2016-04-18 01:41:41 +0000

    indexdone

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,7 +2,8 @@ class UserController < ApplicationController
   #Code found in Ruby on Rails Tutorial by Michael Hartl
   #Chapter 9. Listing 9.42
   #https://www.railstutorial.org/book/updating_and_deleting_users
+  before_filter :authenticate_admin!, :except => [:show]
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.paginate(page: params[:page],per_page: 8)
   end
 end
